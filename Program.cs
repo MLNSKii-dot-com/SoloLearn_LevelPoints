d9195511ce79b48e3fd9d0e0a35565cd3fdd0375
MLNSKii-dot-com <62978157+MLNSKii-dot-com@users.noreply.github.com>
2022-07-25 18:25:02 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿//Passing the first level of a video game awards the player 1 point.For each subsequent level passed, the points awarded increment by 1 (2 for the 2nd level, 3 for the 3rd, and so on).
+//The program you are given takes the number of passed levels as input.Complete the given function to take that number as an argument, and recursively calculate and return the total number of points given for all passed levels.
+
+//Sample Input
+//3
+
+//Sample Output
+//6
+
+//Explanation
+//Level 1: 1 point
+//Level 2: 2 points
+//Level 3: 3 points
+//Total: 1+2+3 = 6 points.
+
+using System;
+
+namespace SoloLearn_LevelPoints
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int levels = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine(Points(levels));
+        }
+
+        static int Points(int levels)
+        {
+            //your code goes here            
+            int t = 0;
+            for (int i = 0; i <= levels; i++)
+            {
+                t += i;
+            }
+            return t;
+        }
+    }
+}
